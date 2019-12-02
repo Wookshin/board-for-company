5c9e1d85f96aeb7eb239620c60de1cc4d7081b15
cotlsdnr@hanmail.net <cotlsdnr@hanmail.net>
2019-12-02 23:24:07 +0900

    First

--- src/main/java/com/study/boot06/controller/WebBoardController.java
+++ src/main/java/com/study/boot06/controller/WebBoardController.java
@@ -2,6 +2,7 @@ package com.study.boot06.controller;
 
 import com.study.boot06.domain.WebBoard;
 import com.study.boot06.persistence.WebBoardRepository;
+import com.study.boot06.vo.PageMaker;
 import com.study.boot06.vo.PageVO;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +13,10 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/boards/")
@@ -23,16 +27,35 @@ public class WebBoardController {
     private WebBoardRepository repo;
 
     @GetMapping("/list")
-    public void list(PageVO vo, Model model) {
+    public void list(@ModelAttribute("pageVO") PageVO vo, Model model) {
 
         Pageable page = vo.makePageable(0, "bno");
 
-        Page<WebBoard> result = repo.findAll(repo.makePredicate(null, null), page);
+        Page<WebBoard> result = repo.findAll(repo.makePredicate(vo.getType(), vo.getKeyword()), page);
 
 
         log.info("" + page);
         log.info(""+result);
 
-        model.addAttribute("result", result);
+        log.info("TOTAL PAGE NUMBER: " + result.getTotalPages());
+
+        model.addAttribute("result", new PageMaker(result));
+    }
+
+    @GetMapping("/register")
+    public void registerGET(@ModelAttribute("vo") WebBoard vo){
+        log.info("register get");
+    }
+
+    @PostMapping("/register")
+    public String registerPOST(@ModelAttribute("vo") WebBoard vo, RedirectAttributes rttr){
+
+        log.info("register post");
+        log.info("" + vo);
+
+        repo.save(vo);
+        rttr.addFlashAttribute("msg", "success");
+
+        return "redirect:/boards/list";
     }
 }
